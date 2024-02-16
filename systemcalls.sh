1c03c2b672c1403772f7cd213a9f523768c9473a
Twesha Thakur <115391165+TweshaThakur@users.noreply.github.com>
2024-02-15 21:09:26 -0800

    added program 2

--- systemcalls.sh
+++ systemcalls.sh
@@ -7,4 +7,12 @@ write (1,"Hello",3);
 }
 ----------------------------------------------------------------------------
 #program 2
+#include <unistd.h>
+#include <stdio.h>
+int main()
+{
+int n;
+n=write(1,"Hello\n",6);
+printf("value of n is %d\n",n);
+}
 
