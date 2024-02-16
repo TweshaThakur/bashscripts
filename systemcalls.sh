cb0d2bc3c48058ef3f7459873be2ff01816fac52
Twesha Thakur <115391165+TweshaThakur@users.noreply.github.com>
2024-02-15 21:27:51 -0800

    4 done 5 to be done

--- systemcalls.sh
+++ systemcalls.sh
@@ -15,4 +15,46 @@ int n;
 n=write(1,"Hello\n",6);
 printf("value of n is %d\n",n);
 }
+----------------------------------------------------------------------------
+#program 3
+#include <unistd.h>
+int main()
+{
+int n;
+char b[30];
+n=read(0,b,30);
+write(1,b,n);
+}
+------------------------------------------------------------------------
+#program 4 
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main()
+{
+int n,fd;
+char buf[50];
+fd =open("test.txt",O_RDONLY);
+n=read(fd,buf,10);
+write(1,buf,10);
+}
+--------------------------------------------------------------------------
+#program 5
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main()
+{
+int n,fd,fd1;
+char buf[50];
+fd =open("test.txt",O_RDONLY);
+n=read(fd,buf,10);
+write(1,buf,10);
+}
+
+
 
